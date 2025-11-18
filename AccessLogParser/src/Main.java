64f0b094e6446fb12d1de23783f3759bb9903447
acer <alukardmasson0@gmail.com>
2025-11-18 22:24:06 +0300

    Добавлен код с арифметичекими действиями

--- AccessLogParser/src/Main.java
+++ AccessLogParser/src/Main.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:" + Math.random());
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int firstNumber = 5;
+        int secondNumber = 10;
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+
+        System.out.println(firstNumber+secondNumber);
+        System.out.println(firstNumber-secondNumber);
+        System.out.println(firstNumber*secondNumber);
+        System.out.println((double)firstNumber/secondNumber);
     }
 }
+
