d1bdcaa952f03f6f053714dbf017978dfb156b35
acer <alukardmasson0@gmail.com>
2025-11-20 22:46:22 +0300

    Исправлена логика вычислеия чисел

--- AccessLogParser/src/Main.java
+++ AccessLogParser/src/Main.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int firstNumber = 5;
-        int secondNumber = 10;
-        System.out.println(firstNumber);
-        System.out.println(secondNumber);
 
-        System.out.println(firstNumber+secondNumber);
-        System.out.println(firstNumber-secondNumber);
-        System.out.println(firstNumber*secondNumber);
-        System.out.println((double)firstNumber/secondNumber);
+        System.out.println("Введите число: ");
+        int firstNumber = new Scanner(System.in) .nextInt(); // вывод в консоль первого числа
+
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in) .nextInt(); // вывод в консоль второго числа
+
+        System.out.println(firstNumber+secondNumber); //вывод в консоль суммы
+        System.out.println(firstNumber-secondNumber); // вывод в консоль разности
+        System.out.println(firstNumber*secondNumber); // вывод в консоль произведения
+        System.out.println((double) firstNumber/secondNumber); // присвоение int(целочисленный тип)
+        // double(вещественного типа), чтобы вычислял дробные и вывод в консоль частного
     }
 }
 
